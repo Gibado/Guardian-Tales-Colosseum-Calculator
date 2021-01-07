959cf4d4318b16d14225e1d645fd80031ccc4a30
Tyler Studanski <Tyler.Studanski@MSPMAC.ORG>
2021-01-07 15:10:50 -0600

    Battles now work and update the page correctly

--- JS/model.js
+++ JS/model.js
@@ -5,22 +5,33 @@ var model = {
     // Pulls team details from the page
     getTeam: function(div) {
         var numbers = div.getElementsByTagName('input');
-        return Team.createTeam(parseInt(numbers[0].value), parseInt(numbers[1].value), parseInt(numbers[2].value));
+        return Team.createTeam(parseFloat(numbers[0].value), parseFloat(numbers[1].value), parseFloat(numbers[2].value));
     },
 
     // Calculates how quickly all characters will die
     battle: function() {
+        var fTeam = this.friendlyTeam.clone();
+        var eTeam = this.enemyTeam.clone();
 
+        var fWinSpeed = eTeam.toughness() / fTeam.dps();
+        var eWinSpeed = fTeam.toughness() / eTeam.dps();
+
+        return {
+            name: fWinSpeed < eWinSpeed ? 'friendly team' : 'enemy team',
+            speed: fWinSpeed < eWinSpeed ? fWinSpeed : eWinSpeed
+        };
     },
 
-    // Updates the model teams
+    // Updates the model teams and battle results
     update: function() {
         this.friendlyTeam = this.getTeam(document.getElementById('myTeam'));
         this.enemyTeam = this.getTeam(document.getElementById('enemyTeam'));
+
+        this.updateResults(this.battle());
     },
 
     // Updates the page with battle results
     updateResults: function(results) {
-
+        document.getElementById('banner').textContent = 'The winner is ' + results.name + ' in ' + results.speed + ' seconds!';
     }
 };
